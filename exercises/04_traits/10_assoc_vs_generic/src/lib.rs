9976755baecc1184fabc17d27b4574db9eec55b3
Felix Kunzmann <felix.kunzmann@eas.iis.fraunhofer.de>
2025-08-11 11:35:16 +0200

    Solve 04/10

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -1,17 +1,36 @@
-// TODO: Define a new trait, `Power`, that has a method `power` that raises `self`
-//  to the power of `n`.
-//  The trait definition and its implementations should be enough to get
-//  the tests to compile and pass.
-//
-// Recommendation: you may be tempted to write a generic implementation to handle
-// all cases at once. However, this is fairly complicated and requires the use of
-// additional crates (i.e. `num-traits`).
-// Even then, it might be preferable to use a simple macro instead to avoid
-// the complexity of a highly generic implementation. Check out the
-// "Little book of Rust macros" (https://veykril.github.io/tlborm/) if you're
-// interested in learning more about it.
-// You don't have to though: it's perfectly okay to write three separate
-// implementations manually. Venture further only if you're curious.
+trait Power<E = Self> {
+    fn power(self, n: E) -> Self;
+}
+
+impl Power for u32 {
+    fn power(self, n: Self) -> Self {
+        let mut power = 1;
+        for _ in 1..=n {
+            power *= self;
+        }
+        power
+    }
+}
+
+impl Power<u16> for u32 {
+    fn power(self, n: u16) -> Self {
+        let mut power = 1;
+        for _ in 1..=n {
+            power *= self;
+        }
+        power
+    }
+}
+
+impl Power<&u32> for u32 {
+    fn power(self, n: &u32) -> Self {
+        let mut power = 1;
+        for _ in 1..=*n {
+            power *= self;
+        }
+        power
+    }
+}
 
 #[cfg(test)]
 mod tests {
